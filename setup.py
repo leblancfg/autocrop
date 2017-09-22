bff8a2acbdee303e5c28f465b81ac8d2e10beb2e
Francois Leblanc <leblancfg@gmail.com>
2017-09-22 15:18:23 -0400

    Add necessary overhead to put this on PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'autocrop',
+  packages = ['autocrop'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Automatically crops faces from batches of pictures',
+  author = 'Francois Leblanc',
+  author_email = 'leblancfg@gmail.com',
+  url = 'https://github.com/leblancfg/autocrop', # use the URL to the github repo
+  download_url = 'https://github.com/leblancfg/autocrop/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['face detection', 'crop', 'autocrop', 'id picture', 'face crop'], # arbitrary keywords
+  classifiers = [],
+)
