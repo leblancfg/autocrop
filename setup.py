e287867256a9d4937615205a5b9ff818315b996a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-03-23 19:29:22 +0200

    Add support for Python 3.8 (#95)
    * Add support for Python 3.8

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
     # $ setup.py publish support.
