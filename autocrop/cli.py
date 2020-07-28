8533722fb41653bedf350b322bbf047fa56b2bf4
Developt <justmins@korea.ac.kr>
2020-07-28 16:20:15 +0900

    Update cli.py

--- autocrop/cli.py
+++ autocrop/cli.py
@@ -198,6 +198,7 @@ def confirmation(question):
         notification_str = "Please respond with 'y' or 'n'"
         print(notification_str)
 
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -205,6 +206,7 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def parse_args(args):
     """Helper function. Parses the arguments given to the CLI."""
