ba2e3309d09c81d79e2a24d98a4734229b1e9b50
leblancfg <leblancfg@gmail.com>
2022-02-11 08:50:09 -0500

    Grab extensions dynamically from PIL

--- autocrop/constants.py
+++ autocrop/constants.py
@@ -1,3 +1,5 @@
+from PIL import Image
+
 FIXEXP = True  # Flag to fix underexposition
 MINFACE = 8  # Minimum face size ratio; too low and we get false positives
 INCREMENT = 0.06
@@ -6,3 +8,6 @@ GAMMA = 0.90
 FACE_RATIO = 6  # Face / padding ratio
 QUESTION_OVERWRITE = "Overwrite image files?"
 CASCFILE = "haarcascade_frontalface_default.xml"
+
+PILLOW_FILETYPES = [k for k in Image.registered_extensions().keys()]
+INPUT_FILETYPES = PILLOW_FILETYPES + [s.upper() for s in PILLOW_FILETYPES]
