c84a04d1dbd69b901982038eb9974236917e11ef
leblancfg <leblancfg@gmail.com>
2022-02-17 20:58:52 -0500

    Add test with picture with transparency

--- tests/test_autocrop.py
+++ tests/test_autocrop.py
@@ -123,7 +123,7 @@ def test_face_percent(face_percent):
 
 def test_transparent_png(integration):
     c = Cropper()
-    img = c.crop("tests/test/transparent.png")
+    img = c.crop("tests/test/expo_67.png")
 
     # Make sure the first pixel is transparent
     assert img[0, 0, 3] == 0
