8f260783d5b70be335e70fea75e7427942e68668
leblancfg <leblancfg@gmail.com>
2022-02-17 20:58:52 -0500

    Add test with picture with transparency

--- autocrop/autocrop.py
+++ autocrop/autocrop.py
@@ -47,10 +47,13 @@ def distance(pt1, pt2):
 
 def bgr_to_rbg(img):
     """Given a BGR (cv2) numpy array, returns a RBG (standard) array."""
-    dimensions = len(img.shape)
-    if dimensions == 2:
+    # Don't do anything for grayscale images
+    if img.ndim == 2:
         return img
-    return img[..., ::-1]
+
+    # Flip the channels. Use explicit indexing in case RGBA is used.
+    img[:, :, [0, 1, 2]] = img[:, :, [2, 1, 0]]
+    return img
 
 
 def gamma(img, correction):
@@ -167,6 +170,7 @@ class Cropper:
         # Scale the image
         try:
             img_height, img_width = image.shape[:2]
+            breakpoint()
         except AttributeError:
             raise ImageReadError
         minface = int(np.sqrt(img_height**2 + img_width**2) / MINFACE)
