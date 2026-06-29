7d44ba85bf433af090591c51c81c266d98d3b4ee
François Leblanc <leblancfg@gmail.com>
2026-06-29 12:20:02 -0400

    Remove Haar cascade backend

--- autocrop/constants.py
+++ autocrop/constants.py
@@ -1,13 +1,8 @@
 from PIL import Image
 
-FIXEXP = True  # Flag to fix underexposition
-MINFACE = 8  # Minimum face size ratio; too low and we get false positives
-INCREMENT = 0.06
 GAMMA_THRES = 0.001
 GAMMA = 0.90
-FACE_RATIO = 6  # Face / padding ratio
 QUESTION_OVERWRITE = "Overwrite image files?"
-CASCFILE = "haarcascade_frontalface_default.xml"
 YUNET_MODEL = "face_detection_yunet_2023mar.onnx"
 
 PILLOW_FILETYPES = [k for k in Image.registered_extensions().keys()]
